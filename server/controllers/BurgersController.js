2c7a81b935e496a9dcc255170851e78db75f52ad
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-26 14:29:12 -0600

    burgers get successful

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -1,16 +1,19 @@
 import BaseController from '../utils/BaseController'
+import { burgersService } from '../services/BurgersService'
 
 export class BurgersController extends BaseController {
-   constructor() {
-      super('api/burgers')
-      this.router
-         .get('', this.getAll)
-   }
-   getAll(req, res, next) {
-      try {
-         const burgers = burgersService.getAll()
-      } catch (error) {
-         next(error)
-      }
-   }
+  constructor() {
+    super('api/burgers')
+    this.router
+      .get('', this.getAll)
+  }
+
+  getAll(req, res, next) {
+    try {
+      const burgers = burgersService.getAll()
+      res.send(burgers)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
